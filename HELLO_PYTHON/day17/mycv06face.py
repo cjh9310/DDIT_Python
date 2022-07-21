07b3653b6afae9493c7ac072f326547be3fb1eb6
DESKTOP-SS23CQV\pc-22 <d>
2022-07-21 09:10:35 +0900

    AI

--- HELLO_PYTHON/day17/mycv06face.py
+++ HELLO_PYTHON/day17/mycv06face.py
@@ -1,16 +1,20 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 
-                                     'haarcascade_frontalface_alt.xml')
-# 이미지 불러오기
-img = cv2.imread('startup.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# 얼굴 찾기
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-# 영상 출력
-cv2.imshow('image', img)
+cascade_file = 'cascade/haarcascade_eye.xml'
+cascade = cv2.CascadeClassifier(cascade_file)
 
+#검출하기
+img = cv2.imread('startup.png') 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+face_list = cascade.detectMultiScale(gray, minSize = (3,3)) 
+
+for (x, y, w, h) in face_list:
+    color = (0, 0, 225) 
+    pen_w = 3 
+    cv2.rectangle(img, (x, y), (x+w, y+h), color, thickness = pen_w) 
+    
+cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+cv2.imshow('img', img)
+cv2.imwrite('temp.jpg', img) 
 cv2.waitKey(0)
 cv2.destroyAllWindows()

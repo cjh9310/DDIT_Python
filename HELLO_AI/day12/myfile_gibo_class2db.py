c122491972970184b7d52c3a17888183dab3fe67
DESKTOP-SS23CQV\pc-22 <d>
2022-08-01 12:26:43 +0900

    AI_omok

--- HELLO_AI/day12/myfile_gibo_class2db.py
+++ HELLO_AI/day12/myfile_gibo_class2db.py
@@ -1,15 +1,34 @@
 import numpy as np
+from day12.dao_gibo import DaoGibo
 
 
 class RaoGibo :
     def __init__(self):
-        self.flagWb = True
+        self.dg = DaoGibo()
+        self.gibo = []
+        self.gibo_ai = []
+        self.ans = []
+        self.win= -1
         
+        self.flagWb = True
         self.arr2D = np.zeros((20,20),dtype="int")
+        
+        self.gibo.append(self.oneline(self.arr2D))
+        self.gibo_ai.append(self.oneline_ai(self.arr2D))
+        
+        
         self.gibo2db()
+        self.insert_db()
+        
+    def insert_db(self):
+        mymax=self.dg.getPanMax()
+        cnt = self.dg.insert("1", self.win, self.gibo, self.gibo_ai, self.ans)
+        print("cnt",cnt)
 
     def getGibo(self):
         file_name = "0_0_1_2.psq"
+#        file_name = "0_0_1_2.psq"
+        
         arr_i=[]
         arr_j=[]
         f = open(file_name, 'r')
@@ -34,9 +53,30 @@ class RaoGibo :
         for i in arr2D:
             for j in i:
                 mystr += str(j)
-        return mystrx  1
+        return mystr
+    
+    def oneline_ai(self,arr2D):
+        mystr = ""
+        for i in arr2D:
+            for j in i:
+                mystr += str(j)
+                
+        if self.flagWb:
+            mystr = mystr.replace("1", "x").replace("2","1")
+        else :
+            mystr = mystr.replace("2", "x")
+        
+        return mystr
+    
+    
     def gibo2db(self):
         arr_i,arr_j = self.getGibo()
+        win = -1
+        if len(arr_i)%2 == 0 :
+            win = 2
+        else :
+            win = 1
+        self.win = win
         for idx,i in enumerate(arr_i):
             ii = arr_i[idx]
             jj = arr_j[idx]
@@ -45,7 +85,13 @@ class RaoGibo :
             else :
                 self.arr2D[ii][jj]=2
             str = self.oneline(self.arr2D)
-            print(idx,ii,jj,str[0:40])
+            str_ai = self.oneline_ai(self.arr2D)
+            
+            self.gibo.append(str)
+            self.gibo_ai.append(str_ai)
+            self.ans.append(ii*20+jj)
+            
+            print(idx,ii,jj,str,str_ai, ii*20+jj,win)
             
             self.flagWb = not self.flagWb   # flagWb가 false로 바뀌어 다음 돌 차례
         
---
+++ HELLO_AI/day13/omok_load.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+omok_answer = np.load("omok_answer.npy")
+omok_train = np.load("omok_train.npy")
+
+print(omok_answer.shape)
+print(omok_train.shape)

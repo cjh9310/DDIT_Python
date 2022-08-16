ca0605942e0a2974d94e9b9723cb49c4a1b63bb9
DESKTOP-SS23CQV\pc-22 <d>
2022-08-16 09:45:34 +0900

    python_Strike

--- HELLO_AI/day15_android/mytflite_cnn_straik.py
+++ HELLO_AI/day15_android/mytflite_cnn_straik.py
@@ -7,20 +7,36 @@ import numpy as np
 
 
 train_images_a = [
-    [0,0],
-    [1,0],
-    [0,1],
-    [1,1],
+    [0,0,0,0],
+    [0,1,0,0],
+    [1,0,0,0],
+    [1,1,0,0],
+
+    [0,0,0,1],
+    [0,1,0,1],
+    [1,0,0,1],
+    [1,1,0,1],
+    
+    [0,0,1,0],
+    [0,1,1,0],
+    [1,0,1,0],
+    [1,1,1,0],
+
+    [0,0,1,1],
+    [0,1,1,1],
+    [1,0,1,1],
+    [1,1,1,1],
 ]
 train_labels_a = [
-    [0,0],
-    [1,0],
-    [0,1],
-    [1,1],
+    2,1,1,0,
+    1,2,0,1,
+    1,0,2,1,
+    0,1,1,2
+
 ]
 
 train_images = np.array(train_images_a,dtype=np.float16)
-train_images = np.reshape(train_images,(4,2))
+train_images = np.reshape(train_images,(16,4))
 
 train_labels = np.array(train_labels_a) 
 
@@ -30,7 +46,7 @@ model = keras.Sequential([
     keras.layers.Flatten(input_shape=(4,1)),
     keras.layers.Dense(512, activation=tf.nn.relu),
     keras.layers.Dense(512, activation=tf.nn.relu),
-    keras.layers.Dense(4)
+    keras.layers.Dense(3)
 ])
 
 
@@ -48,7 +64,7 @@ predictions = model.predict(train_images)
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-f = open('mnist_cnn_gawi2.tflite', "wb")
+f = open('mnist_cnn_strike.tflite', "wb")
 f.write(tflite_model)
 f.close()
 

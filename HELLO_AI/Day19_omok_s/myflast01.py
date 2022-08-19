c083091f6aafef4d7c3338891691b44867158857
cjh9310 <mine931045@gmail.com>
2022-08-19 16:08:33 +0900

    AI_vs_AI

---
+++ HELLO_AI/Day19_omok_s/myflast01.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask.globals import request
+from flask.templating import render_template
+from flask.json import jsonify
+
+app = Flask(__name__,static_url_path='')
+
+@app.route('/')
+def test():
+    return render_template('test.html')
+
+@app.route('/ajax', methods=['POST'])
+def ajaxmod():
+    stone = request.form['stone']
+    data400 = request.form['data400']
+    print("stone",stone)
+    print("data400",data400)
+    return jsonify({'i':0,'j':1})
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, host="0.0.0.0")
+    
--- HELLO_AI/Day19_omok_t/myflast01.py
+++ HELLO_AI/Day19_omok_t/myflast01.py
@@ -5,13 +5,18 @@ from flask.json import jsonify
 
 app = Flask(__name__,static_url_path='')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
 @app.route('/')
-@app.route('/omok4_20')
+@app.route('/omok')
 def omok():
-    return render_template('omok4_20.html')
+    return render_template('omok4_20_teacher.html')
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
     

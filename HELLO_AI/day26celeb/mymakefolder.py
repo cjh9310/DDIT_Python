da9f7523d4110e8b8549ef419764135624036f6e
cjh9310 <mine931045@gmail.com>
2022-08-29 12:26:05 +0900

    AI_faceRecognition

---
+++ HELLO_AI/day26celeb/mymakefolder.py
@@ -0,0 +1,5 @@
+import os
+
+for i in range(100,122):
+
+    os.makedirs("train_image/"+str(i))
---
+++ HELLO_AI/day26celeb/step1_myfaces_32_32.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2
+import random
+import os
+
+labels = ["강동원",
+          "공유",
+          "권오중",
+          "김종국",
+          "리노",
+          
+          "마동석",
+          "박명수",
+          "박성웅",
+          "박은빈",
+          "수지",
+          
+          "아이유",
+          "안유진",
+          "예리",
+          "오연서",
+          "원빈",
+          
+          "장원영",
+          "정소민",
+          "조이",
+          "채수빈",
+          "천우희",
+          
+          
+          "태연",
+          "현빈"
+          ]
+def originTo3232(from_file,to_file):
+    img_array = np.fromfile(from_file , np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    resize_img = cv2.resize(img, (32, 32))
+    cv2.imwrite(to_file , resize_img)
+for i in range(22):
+    files = os.listdir("train_origin/{}".format(labels[i]))
+    for idx,f in enumerate(files):
+        print("train_origin/{}/{}".format(labels[i],f), "train_image/{}/{}.png".format(100+i,idx))
+        originTo3232("train_origin/{}/{}".format(labels[i],f), "train_image/{}/{}.png".format(100+i,idx))
+
+# originTo3232("train_origin/{}/{}".format(labels[0]), "train_image/100/{}.png".format(0))
+# originTo3232("train_origin/{}/{}".format(labels[1]), "train_image/100/{}.png".format(1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
